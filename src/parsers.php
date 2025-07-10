81c353adde1a67bbe522f677a6f5d3716cb55a32
stav86 <averin.stepan16@yandex.ru>
2025-07-11 00:18:27 +0500

    correct

--- src/parsers.php
+++ src/parsers.php
@@ -6,25 +6,6 @@ use Symfony\Component\Yaml\Yaml;
 
 function parseData($input)
 {
-    if (is_array($input)) {
-        return $input;
-    }
-    if (!is_string($input)) {
-        return [$input];
-    }
-    $input = trim($input);
-    if ($input === '') {
-        return [];
-    }
-    if (is_numeric($input)) {
-        return [(float)$input];
-    } elseif (strtolower($input) === 'true') {
-        return [true];
-} elseif (strtolower($input) === 'false') {
-        return [false];
-    } elseif (strtolower($input) === 'null') {
-        return [null];
-    }
     if (file_exists($input)) {
         $extension = pathinfo($input, PATHINFO_EXTENSION);
         $data = file_get_contents($input);
@@ -42,7 +23,7 @@ function parseData($input)
                 throw new RuntimeException("JSON decode error: " . json_last_error_msg());
             }
             return $decodedData;
-             },
+        },
         'yaml' => function ($data) {
             if (!is_string($data)) {
                 throw new RuntimeException("Expected string for YAML parsing.");
