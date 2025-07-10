3891e7fdb9a11f054e8140d6f7243bff69306b33
stav86 <averin.stepan16@yandex.ru>
2025-07-11 00:12:57 +0500

    correct

--- src/parsers.php
+++ src/parsers.php
@@ -9,6 +9,22 @@ function parseData($input)
     if (is_array($input)) {
         return $input;
     }
+    if (!is_string($input)) {
+        return [$input];
+    }
+    $input = trim($input);
+    if ($input === '') {
+        return [];
+    }
+    if (is_numeric($input)) {
+        return [(float)$input];
+    } elseif (strtolower($input) === 'true') {
+        return [true];
+} elseif (strtolower($input) === 'false') {
+        return [false];
+    } elseif (strtolower($input) === 'null') {
+        return [null];
+    }
     if (file_exists($input)) {
         $extension = pathinfo($input, PATHINFO_EXTENSION);
         $data = file_get_contents($input);
@@ -23,14 +39,14 @@ function parseData($input)
                 throw new RuntimeException("JSON decode error: " . json_last_error_msg());
             }
             return $decodedData;
-        },
+             },
         'yaml' => function ($data) {
             return Yaml::parse($data);
         },
         'yml' => function ($data) {
             return Yaml::parse($data);
         },
-        ];
+    ];
     if (!array_key_exists($extension, $parsers)) {
         return [$data];
     }
