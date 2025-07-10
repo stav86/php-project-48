01667e13d9b560ed1adeb8f1a07b952f3e49c07c
stav86 <averin.stepan16@yandex.ru>
2025-07-10 23:24:59 +0500

    correct

--- src/parsers.php
+++ src/parsers.php
@@ -18,29 +18,24 @@ function parseData($input)
         throw new InvalidArgumentException("Input must be a string.");
     }
     $parsers = [
-        'json' => function ($file) {
-            $data = json_decode(file_get_contents($file), true);
+        'json' => function ($data) {
+            $decodedData = json_decode($data, true);
             if (json_last_error() !== JSON_ERROR_NONE) {
                 throw new RuntimeException("JSON decode error: " . json_last_error_msg());
             }
-            return $data;
+            return $decodedData;
         },
-        'yaml' => function ($file) {
-            return Yaml::parse(file_get_contents($file));
+        'yaml' => function ($data) {
+            return Yaml::parse($data);
         },
-        'yml' => function ($file) {
-            return Yaml::parse(file_get_contents($file));
+        'yml' => function ($data) {
+            return Yaml::parse($data);
         },
     ];
-    $parser = array_values(array_filter(
-        $parsers,
-        fn ($key) => $key === $extension,
-        ARRAY_FILTER_USE_KEY
-    ))[0];
     if (!array_key_exists($extension, $parsers)) {
         throw new InvalidArgumentException("Unsupported file extension: $extension");
     }
-    $result = $parsers[$extension]($filePath);
+    $result = $parsers[$extension]($data);
     if (!is_array($result)) {
         throw new RuntimeException("Parsed data is not an array.");
     }
