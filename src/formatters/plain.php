a98c57c260334357a8d5d9380ba3dfaef5f6958a
stav86 <averin.stepan16@yandex.ru>
2025-07-13 10:47:55 +0500

    correct plain

--- src/formatters/plain.php
+++ src/formatters/plain.php
@@ -26,27 +26,18 @@ function getPlainFormat(array $diff, string $prefix = ''): array
     return array_reduce(array_keys($diff), function ($result, $key) use ($diff, $prefix) {
         $fullPath = getFullPath($prefix, $key);
         $item = $diff[$key];
-        switch ($item['status']) {
-            case 'added':
-                $value = formatValue($item['value']);
-                return array_merge($result, ["Property '$fullPath' was added with value: $value"]);
-                break;
-            case 'removed':
-                return array_merge($result, ["Property '$fullPath' was removed"]);
-                break;
-            case 'changed':
-                $oldValue = formatValue($item['old_value']);
-                $newValue = formatValue($item['new_value']);
-                return array_merge($result, ["Property '$fullPath' was updated. From $oldValue to $newValue"]);
-                break;
-            case 'nested':
-                $nestedResult = getPlainFormat($item['children'], $fullPath);
-                return array_merge($result, [implode("\n", $nestedResult)]);
-                break;
-            case 'unchanged':
-                break;
-            default:
-                throw new \InvalidArgumentException("Unknown status '{$item['status']}' for property '$fullPath'");
+        if ($item['status'] === 'added') {
+            $value = formatValue($item['value']);
+            return array_merge($result, ["Property '$fullPath' was added with value: $value"]);
+        } elseif ($item['status'] === 'removed') {
+            return array_merge($result, ["Property '$fullPath' was removed"]);
+        } elseif ($item['status'] === 'changed') {
+            $oldValue = formatValue($item['old_value']);
+            $newValue = formatValue($item['new_value']);
+            return array_merge($result, ["Property '$fullPath' was updated. From $oldValue to $newValue"]);
+        } elseif ($item['status'] === 'nested') {
+            $nestedResult = getPlainFormat($item['children'], $fullPath);
+            return array_merge($result, [implode("\n", $nestedResult)]);
         }
         return $result;
     }, []);
